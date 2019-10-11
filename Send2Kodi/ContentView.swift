d1af7a4aad1a95050d52cfe975b4935f2107d9f3
Erwan Lacoste <erwan.lacoste+git@gmail.com>
2019-10-11 21:35:04 +0200

    Initial Commit

---
+++ Send2Kodi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Send2Kodi
+//
+//  Created by Erwan Lacoste on 11/10/2019.
+//  Copyright © 2019 Erwan Lacoste. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
