7d9895b3781228e0507433d3a496c0d9130003fc
Erwan Lacoste <erwan.lacoste+git@gmail.com>
2020-01-24 16:56:03 +0100

    extract Youtube URL from web page

--- SendYouTube2Kodi/Action.js
+++ SendYouTube2Kodi/Action.js
@@ -14,32 +14,16 @@ Action.prototype = {
         // Here, you can run code that modifies the document and/or prepares
         // things to pass to your action's native code.
         
-        // We will not modify anything, but will pass the body's background
-        // style to the native code.
-        
-        console.log('@@@@@@@@@@@ kodi ext run', arguments);
-        
-        
-        arguments.completionFunction({ "currentBackgroundColor" : document.body.style.backgroundColor })
+        // just pass the URL to the native code
+        arguments.completionFunction({ "currentUrl" : window.location.href })
     },
     
     finalize: function(arguments) {
         // This method is run after the native code completes.
-        
-        // We'll see if the native code has passed us a new background style,
-        // and set it on the body.
-        
-        console.log('@@@@@@@@@@@ kodi ext finalize', arguments);
-        
-        var newBackgroundColor = arguments["newBackgroundColor"]
-        if (newBackgroundColor) {
-            // We'll set document.body.style.background, to override any
-            // existing background.
-            document.body.style.background = newBackgroundColor
-        } else {
-            // If nothing's been returned to us, we'll set the background to
-            // blue.
-            document.body.style.background= "blue"
+        // it passes a boolean to stop the video
+        if (arguments) {
+            // long shot, let's try to stop the video:
+            document.getElementById('movie_player').stopVideo()
         }
     }
     
--- SendYouTube2Kodi/ActionRequestHandler.swift
+++ SendYouTube2Kodi/ActionRequestHandler.swift
@@ -17,7 +17,7 @@ class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
         // Do not call super in an Action extension with no user interface
         self.extensionContext = context
         
-        var found = false
+        var finished = false
         
         // Find the item containing the results from the JavaScript preprocessing.
         outer:
@@ -35,6 +35,7 @@ class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
                                     kodiService.send2kodi(id: youtubeId)
                                 }
                             })
+                            break outer
                         }
                         
                         if itemProvider.hasItemConformingToTypeIdentifier(String(kUTTypePropertyList)) {
@@ -44,37 +45,60 @@ class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
                                     self.itemLoadCompletedWithPreprocessingResults(dictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! [String: Any]? ?? [:])
                                 }
                             })
-                            found = true
+                            finished = true
                             break outer
                         }
                     }
                 }
         }
         
-        if !found {
-            self.doneWithResults(nil)
+        if !finished {
+            self.finishIt()
         }
     }
     
+    // Make sure you finish the job here!!
     func itemLoadCompletedWithPreprocessingResults(_ javaScriptPreprocessingResults: [String: Any]) {
         // Here, do something, potentially asynchronously, with the preprocessing
         // results.
         
-        // In this very simple example, the JavaScript will have passed us the
-        // current background color style, if there is one. We will construct a
-        // dictionary to send back with a desired new background color style.
-        
-        print("##### kodi ext load \(javaScriptPreprocessingResults)")
-        
-        
-        let bgColor: Any? = javaScriptPreprocessingResults["currentBackgroundColor"]
-        if bgColor == nil ||  bgColor! as! String == "" {
-            // No specific background color? Request setting the background to red.
-            self.doneWithResults(["newBackgroundColor": "red"])
-        } else {
-            // Specific background color is set? Request replacing it with green.
-            self.doneWithResults(["newBackgroundColor": "green"])
+        var stopPlayback = false
+        // the JavaScript will have passed us the url
+        if let url = javaScriptPreprocessingResults["currentUrl"] as! String? {
+            print("processing send 2 kodi with url: \(url)")
+            
+            let userDefaults = UserDefaults(suiteName: "group.be.vershina.Send2Kodi") ?? .standard
+            let config = KodiConfig(userDefaults)
+            let kodiService = KodiService.init(config: config)
+
+            if let youtubeId = kodiService.extractYoutubeId(url) {
+                kodiService.send2kodi(id: youtubeId)
+                // youtube URL, try to stop playback
+                stopPlayback = true
+            }
         }
+        self.finishIt(stopPlayback: stopPlayback)
+    }
+    
+    func finishIt(stopPlayback: Bool = false) {
+        if (stopPlayback) {
+            // let's callback javascript to try to stop the playback
+            let resultsDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: true]
+            
+            let resultsProvider = NSItemProvider(item: resultsDictionary as NSDictionary, typeIdentifier: String(kUTTypePropertyList))
+            
+            let resultsItem = NSExtensionItem()
+            resultsItem.attachments = [resultsProvider]
+            
+            // Signal that we're complete, returning our results.
+            self.extensionContext!.completeRequest(returningItems: [resultsItem], completionHandler: nil)
+        } else {
+            // We still need to signal that we're done even if we have nothing to
+            // pass back.
+            self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+        }
+        // Don't hold on to this after we finished with it.
+        self.extensionContext = nil
     }
     
     func doneWithResults(_ resultsForJavaScriptFinalizeArg: [String: Any]?) {
